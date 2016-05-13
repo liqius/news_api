6752aa8725c5f0caee7fb43a3d245a6b85068dd1
cospit <cospit@yeah.net>
2016-05-13 20:13:39 +0800

    do some update

--- app.py
+++ app.py
@@ -2,3 +2,15 @@
 import flask, requests
 
 app = flask.app(__name__)
+try:
+    import api.news_get
+
+news = news_get.main()
+if type(news) == dict:
+    @app.route(/)
+    def index():
+        return '''ERROR'''
+else:
+    @app.route(/)
+    def index():
+        
